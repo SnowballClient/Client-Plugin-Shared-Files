0fb471067a1a5bd0bad2b7459d4875e2a3540129
Eric Golde <eric@golde.org>
2018-12-08 16:29:14 -0800

    Removed Auth

--- src/org/golde/snowball/shared/CustomPayloadConstants.java
+++ src/org/golde/snowball/shared/CustomPayloadConstants.java
@@ -3,7 +3,6 @@ package org.golde.snowball.shared;
 public class CustomPayloadConstants {
 
 	public static final String CHANNEL_NAME = "sb";
-	public static final String AUTH = "auth";
 	public static final String RESPAWN = "respawn";
 	
 }
